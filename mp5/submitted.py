d62ffda46c55b61747dd36d1c2954c76589cb8f8
RickyL-2000 <ruiqili4@illinois.edu>
2021-11-17 20:55:48 +0800

    mp5 done

--- mp5/submitted.py
+++ mp5/submitted.py
@@ -1,5 +1,6 @@
 import os, h5py, glob, re, argparse
 import numpy as  np
+from numpy.core.fromnumeric import shape
 
 ###############################################################################
 # Utility functions: rect_regression, sigmoid, conv2, conv_layer
@@ -7,6 +8,14 @@ def rect_regression(rect, anchor):
     '''Convert a rectangle into a regression target, with respect to  a given anchor rect'''
     return(np.array([(rect[0]-anchor[0])/anchor[2],(rect[1]-anchor[1])/anchor[3],
                      np.log(rect[2]/anchor[2]),np.log(rect[3]/anchor[3])]))
+
+def target2rect(regression_target, anchor):
+    rect = np.zeros(4)
+    rect[0] = regression_target[0]*anchor[2]+anchor[0]
+    rect[1] = regression_target[1]*anchor[3]+anchor[1]
+    rect[2] = np.exp(min(np.log(2),regression_target[2]))*anchor[2]
+    rect[3] = np.exp(min(np.log(2),regression_target[3]))*anchor[3]
+    return(rect)
 
 def sigmoid(Xi):
     '''
@@ -115,6 +124,25 @@ def forwardprop(X, W1, W2):
       Yhat[n1,n2,a,:4] - regression output, (n1,n2) pixel, a'th anchor
       Yhat[n1,n2,a,4] - classfication output, (n1,n2) pixel, a'th anchor
     '''
+
+    N1, N2, NC = X.shape
+    M1, M2, NC, ND = W1.shape
+    _, _, ND, NA, NY = W2.shape
+
+    # first layer
+    H = conv_layer(X, W1, 1)
+    H[H < 0] = 0
+
+    # second layer
+    Yhat = np.zeros(shape=(N1, N2, NA, NY))
+    for n1 in range(N1):
+        for k in range(NY):
+            Yhat[n1, :, :, k] = np.dot(H[n1, :, :], W2[0, 0, :, :, k])
+
+    Yhat[:, :, :, 4] = sigmoid(Yhat[:, :, :, 4])
+
+    return H, Yhat
+
     raise RuntimeError("You need to write this!")
 
 
@@ -132,6 +160,16 @@ def detect(Yhat, number_to_return, anchors):
       from regression targets back into rectangles
       (i.e., reverse the process in rect_regression()).
     '''
+    best_rects = np.zeros((number_to_return, 4))
+
+    indices = np.argsort(Yhat[:, :, :, 4].flatten())
+    for i in range(number_to_return):
+        index = np.unravel_index(indices[-1-i], Yhat.shape[:3])
+        rect = target2rect(Yhat[index[0], index[1], index[2], :4], anchors[index[0], index[1], index[2], :4])
+        best_rects[i, :] = rect
+    
+    return best_rects
+
     raise RuntimeError("You need to write this!")
 
 def loss(Yhat, Y):
@@ -152,6 +190,13 @@ def loss(Yhat, Y):
         the classification target is  Y[n1,n2,a,4]==1.  If there are no such targets,
         then mse_loss = 0.
     '''
+    N1,N2,NA,NY = Y.shape
+    mse_loss = 0.5 * np.sum(np.expand_dims(Y[:, :, :, 4], axis=3) * (Y[:, :, :, :4] - Yhat[:, :, :, :4])**2) / np.sum(Y[:, :, :, 4])
+    bce_loss = -1 / N1 / N2 / NA * \
+        np.sum(Y[:, :, :, 4] * safe_log(Yhat[:, :, :, 4]) + (1 - Y[:, :, :, 4]) * safe_log(Yhat[:, :, :, 4]))
+
+    return bce_loss, mse_loss
+
     raise RuntimeError("You need to write this!")
 
 def backprop(Y, Yhat, H, W2):
@@ -166,6 +211,26 @@ def backprop(Y, Yhat, H, W2):
       GradXi1 (N1,N2,ND) - derivative of loss w.r.t. 1st-layer excitations
       GradXi2 (N1,N2,NA,NY) - derivative of loss w.r.t. 2nd-layer excitations
     '''
+    N1,N2,NA,NY = Y.shape
+    N1,N2,ND = H.shape
+
+    # derivative for Yhat
+    # GradXi2 = np.expand_dims(Y[:, :, :, 4], axis=3) * (Yhat - Y) / np.sum(Y[:, :, :, 4])
+    # GradXi2 = (Yhat - Y) / np.sum(Y[:, :, :, 4])
+    # FIXME: why difference only? (no scaling?)
+    GradXi2 = Yhat - Y
+    # GradXi2[:, :, :, 0: 4] *= np.expand_dims(Y[:, :, :, 4], axis=3)
+
+    GradXi1 = np.zeros((N1, N2, ND))
+    # derivative for H
+    for n1 in range(N1):
+        for k in range(NY):
+            GradXi1[n1, :, :] += np.dot(GradXi2[n1, :, :, k], W2[0, 0, :, :, k].T)
+    # derivative for Xi1
+    GradXi1[H <= 0.0] = 0.0
+
+    return GradXi1, GradXi2
+
     raise RuntimeError("You need to write this!")
 
 def weight_gradient(X, H, GradXi1, GradXi2, M1, M2):
@@ -182,6 +247,31 @@ def weight_gradient(X, H, GradXi1, GradXi2, M1, M2):
       dW1 (M1,M2,NC,ND) - gradient of loss w.r.t. layer-1 weights
       dW2 (1,1,ND,NA,NY) -  gradient of loss w.r.t. layer-2 weights
     '''
+    N1,N2,NC = X.shape
+    N1,N2,ND = H.shape
+    N1,N2,NA,NY = GradXi2.shape
+
+    dW1 = np.zeros((M1, M2, NC, ND))
+    dW2 = np.zeros((1, 1, ND, NA, NY))
+
+    for d in range(ND):
+        GradXi1_padded = np.zeros((N1+2, N2+2))
+        GradXi1_padded[1: N1+1, 1:N2+1] = GradXi1[:, :, d]
+        for c in range(NC):
+            X_flattened = X[:, :, c].flatten()  # no flipping
+            for m1 in range(M1):
+                for m2 in range(M2):
+                    dW1[m1, m2, c, d] = np.inner(X_flattened, GradXi1_padded[m1: m1+N1, m2: m2+N2].flatten())
+    # FIXME: why?
+    dW1 = dW1[::-1, ::-1, :, :]
+
+    for d in range(ND):
+        for a in range(NA):
+            for k in range(NY):
+                dW2[0, 0, d, a, k] = np.sum(GradXi2[:, :, a, k] * H[:, :, d])
+
+    return dW1, dW2
+
     raise RuntimeError("You need to write this!")
 
 def weight_update(W1,W2,dW1,dW2,learning_rate):
@@ -196,6 +286,11 @@ def weight_update(W1,W2,dW1,dW2,learning_rate):
       new_W1 (M1,M2,NC,ND) = first layer weights
       new_W2 (1,1,ND,NA,NY) = second layer weights
     '''
+    new_W1 = W1 - learning_rate * dW1
+    new_W2 = W2 - learning_rate * dW2
+
+    return new_W1, new_W2
+
     raise RuntimeError("You need to write this!")
 
 
